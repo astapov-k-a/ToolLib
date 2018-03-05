f94bb1be903e708ab2ad7068e8b69037223debb8
Konstantin, philosophical coding errant <akam.softally@gmail.com>
2018-03-06 00:23:12 +0300

    Update AnyContainer.h

--- AnyContainer.h
+++ AnyContainer.h
@@ -1,4 +1,25 @@
-1
+//#include <utility>
+#include <cstdio>
+#include <new>
+#include <memory>
+#include <cassert>
+
+typedef size_t Size;
+template <typename Tn> using UniquePointer = std::unique_ptr<Tn>;
+
+#define ANY_CONTAINER_UNIVERSAL_ASSERT( Condition, Message, ThrowObject) if ( ! (Condition) ) { assert(0); }
+#define ANY_CONTAINER_CAST_ERROR_MESSAGE_TEXT "AnyCastError"
+
+class Any;
+
+namespace std {
+template <typename ValueType> ValueType any_cast( const Any&  operand );
+template <typename ValueType> ValueType any_cast(       Any&  operand );
+template <typename ValueType> ValueType any_cast(       Any&& operand );
+template <typename ValueType> ValueType any_cast( const Any*  operand );
+template <typename ValueType> ValueType any_cast(       Any*  operand );
+} // namespace std
+
 /**
  * @brief улучшенный аналог std::any, в отличии от стандартной any, знает, какой тип хранит. Цена - виртуальность + хранение имплементации по ссылке
  **/
@@ -8,6 +29,7 @@ class Any {
  
   Any() {}
   Any( const Any & to_copy ) { set_implementation( to_copy.MakeHardCopy() ); }
+  Any( Any && to_move ) { set_implementation( to_move.ReleaseImplementation() );  }
   virtual ~Any() {}    
   Any & operator=( const Any & to_copy ) { 
     set_implementation( to_copy.MakeHardCopy() );
@@ -22,11 +44,12 @@ class Any {
   }
   
   virtual Size GetTypeCode() const{ return implementation() ? implementation()->GetTypeCode() : InvalidTypeCode; }
+  template <typename Tn> static Size GetCodeOf() { return typeid(Tn).hash_code; }
   
  protected:
   template <typename Tn> class AnyImplementation;
   
-  explicit Any( Any * implementation_ptr ) : implementation_( implementation_ptr ) {    
+  Any( Any * implementation_ptr ) : implementation_( implementation_ptr ) {    
   }
   
   Any const *     implementation() const { return implementation_.get(); }
@@ -35,8 +58,9 @@ class Any {
   
   virtual Any  * MakeHardCopy() const { return implementation() ? implementation()->MakeHardCopy() : nullptr; }
   virtual bool CompareWithOrDie( const Any & to_compare ) const { return implementation()->CompareWithOrDie( to_compare ); }
-  template < typename Tn, typename... Args > static Tn * Allocate( Args&&... args ) { return new (std::nothrow) Tn( std::forward<Args>(args)... ); }
-  //template < typename Tn, typename... Args > AnyImplementation<Tn> * AllocateImplementation( Args&&... args ) const { return Allocate< AnyImplementation<Tn> >( std::forward<Args>(args)... ); }
+  template < typename Tn, typename... Args > Tn * Allocate( Args&&... args ) const { return new (std::nothrow) Tn( std::forward<Args>(args)... ); }
+  template < typename Tn, typename... Args > AnyImplementation<Tn> * AllocateImplementation( Args&&... args ) const { return Allocate< AnyImplementation<Tn> >( std::forward<Args>(args)... ); }
+  Any * ReleaseImplementation() { return implementation_.release(); }
   
  private:
   template< class T, class... Args > friend Any MakeAny( Args&&... args );
@@ -56,18 +80,22 @@ template <typename Tn> class Any::AnyImplementation : public Any {
   
  protected:  
   virtual Any  * MakeHardCopy() const { 
-    This * re = AllocateStatic( object_ );
-    //re->object_ = object_;
+    This * re = Any::Allocate< This >();
+    re->object_ = object_;
     return re; 
   }
   virtual bool CompareWithOrDie( const Any & to_compare ) const { return implementation()->CompareWithOrDie( to_compare ); }
-  template < typename... Args > static This * AllocateStatic( Args&&... args ) { 
-    return Any().Allocate< This > ( std::forward<Args>(args)... );
-  }
+  template < typename... Args > static This * AllocateStatic( Args&&... args ) { return new (std::nothrow) This( std::forward<Args>(args)... ); }
  
  private:
   template< class T, class... Args > friend Any MakeAny( Args&&... args );
   friend void AnyDebug();
+  
+  template <typename ValueType> friend ValueType std::any_cast( const Any&  operand );
+  template <typename ValueType> friend ValueType std::any_cast(       Any&  operand );
+  template <typename ValueType> friend ValueType std::any_cast(       Any&& operand );
+  template <typename ValueType> friend ValueType std::any_cast( const Any*  operand );
+  template <typename ValueType> friend ValueType std::any_cast(       Any*  operand );
   
   Type object_;
 };
@@ -80,3 +108,52 @@ Any MakeAny( Args&&... args ) {
 }
 //template< class T, class U, class... Args >
 //Any MakeAny( std::initializer_list<U> il, Args&&... args );
+
+
+namespace std {
+
+template <typename ValueType>
+ValueType any_cast(const Any& operand) {
+  ANY_CONTAINER_UNIVERSAL_ASSERT( operand.GetTypeCode() == Any::GetCodeOf< ValueType >, ANY_CONTAINER_CAST_ERROR_MESSAGE_TEXT "_1" , AnyCastError );
+  typedef Any::AnyImplementation<ValueType> AnyImpl;
+  return dynamic_cast<const AnyImpl *>( operand.implementation() ).object_;
+}
+
+template <typename ValueType>
+ValueType any_cast(Any& operand) {
+  ANY_CONTAINER_UNIVERSAL_ASSERT( operand.GetTypeCode() == Any::GetCodeOf< ValueType >, ANY_CONTAINER_CAST_ERROR_MESSAGE_TEXT "_2" , AnyCastError );
+  typedef Any::AnyImplementation<ValueType> AnyImpl;
+  return dynamic_cast<const AnyImpl *>( operand.implementation() ).object_;
+}
+
+template <typename ValueType>
+ValueType any_cast(Any&& operand) {
+  ANY_CONTAINER_UNIVERSAL_ASSERT( operand.GetTypeCode() == Any::GetCodeOf< ValueType >, ANY_CONTAINER_CAST_ERROR_MESSAGE_TEXT "_3" , AnyCastError );
+  typedef Any::AnyImplementation<ValueType> AnyImpl;
+  return dynamic_cast<const AnyImpl *>( operand.implementation() ).object_;
+}
+
+template <typename ValueType>
+const ValueType* any_cast(const Any* operand) noexcept {
+  ANY_CONTAINER_UNIVERSAL_ASSERT( operand->GetTypeCode() == Any::GetCodeOf< ValueType >, ANY_CONTAINER_CAST_ERROR_MESSAGE_TEXT "_4" , AnyCastError );
+  typedef Any::AnyImplementation<ValueType> AnyImpl;
+  return dynamic_cast<const AnyImpl *>( operand->implementation() ).object_;
+}
+
+template <typename ValueType>
+ValueType* any_cast(Any* operand) noexcept {    
+  ANY_CONTAINER_UNIVERSAL_ASSERT( operand->GetTypeCode() == Any::GetCodeOf< ValueType >, ANY_CONTAINER_CAST_ERROR_MESSAGE_TEXT "_5" , AnyCastError );
+  typedef Any::AnyImplementation<ValueType> AnyImpl;
+  return dynamic_cast<const AnyImpl *>( operand->implementation() ).object_;
+}
+
+} // namespace std
+
+
+
+
+void AnyDebug() {
+  Any::AnyImplementation<int> i;
+  auto * ptr = Any::AnyImplementation<int>::AllocateStatic();
+  Any x( (Any*)ptr);
+}
